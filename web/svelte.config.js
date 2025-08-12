f8cf7962d4622248290e1f4d33a9373e29597a77
Sparrow <benjaminthorpe19@gmail.com>
2025-08-12 13:58:00 +0000

    chore: enable remote functions and async

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -3,10 +3,20 @@ import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://svelte.dev/docs/kit/integrations
-  // for more information about preprocessors
-  preprocess: vitePreprocess(),
-  kit: { adapter: adapter() },
+	// Consult https://svelte.dev/docs/kit/integrations
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
+	compilerOptions: {
+		experimental: {
+			async: true,
+		},
+	},
+	kit: {
+		adapter: adapter(),
+		experimental: {
+			remoteFunctions: true,
+		},
+	},
 };
 
 export default config;
