b2880250ca06d482093db1827da2aa7f9be0681d
Sparrow <benjaminthorpe19@gmail.com>
2025-09-01 11:34:59 +0000

    refactor: API_BASE_URL -> API_ENDPOINT

--- web/src/routes/register/register.remote.ts
+++ web/src/routes/register/register.remote.ts
@@ -1,11 +1,11 @@
 import { form } from '$app/server';
-import { API_BASE_URL } from '$env/static/private';
+import { API_ENDPOINT } from '$env/static/private';
 
 export const register = form(async (formData) => {
 	const form = Object.fromEntries(formData);
 
 	try {
-		const res = await fetch(`${API_BASE_URL}/api/v1/users`, {
+		const res = await fetch(`${API_ENDPOINT}/api/v1/users`, {
 			method: 'POST',
 			headers: { 'Content-Type': 'application/json' },
 			body: JSON.stringify(form)
