f212b9898fca92a9f3909de60db4efd69476ba01
Sparrow <benjaminthorpe19@gmail.com>
2025-09-01 12:18:32 +0000

    chore: update tsconfig (#64)
    * chore: update tsconfig
    
    * refactor: API_BASE_URL -> API_ENDPOINT
    
    * chore: modifications, update lefthook.yml
    
    * rename: move folders into apps directory
    
    * fixes: pre-commit hook not running due to biome, other upgrades/updates
    
    * chore: update lefthook file
    
    * update README

--- apps/web/src/routes/login/login.remote.ts
+++ apps/web/src/routes/login/login.remote.ts
@@ -1,11 +1,11 @@
 import { form } from "$app/server";
-import { API_BASE_URL } from "$env/static/private";
+import { API_ENDPOINT } from "$env/static/private";
 
 export const login = form(async (formData) => {
 	const form = Object.fromEntries(formData);
 
 	try {
-		const res = await fetch(`${API_BASE_URL}/api/v1/auth/token`, {
+		const res = await fetch(`${API_ENDPOINT}/api/v1/auth/token`, {
 			method: "POST",
 			headers: { "Content-Type": "application/json" },
 			body: JSON.stringify(form),
