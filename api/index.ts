f77a5048dbb04db9601c8b5dc99eb7b957002d37
Ben <bnjox@users.noreply.github.com>
2026-01-28 13:16:29 +0000

    remove all files in codebase folder

--- api/index.ts
+++
@@ -1,8 +0,0 @@
-import { serve } from "@hono/node-server";
-
-import app from "./src/app";
-import { config } from "./src/config/config";
-
-serve({ fetch: app.fetch, port: config.PORT }, (info) => {
-	console.log(`Server is running on port :${info.port}`);
-});
--- api/src/validators/users.ts
+++
@@ -1,1 +0,0 @@
-// validate users incoming data
